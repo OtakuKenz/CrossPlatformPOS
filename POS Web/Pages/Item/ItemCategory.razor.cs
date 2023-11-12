4ab03638e0bb6f02e79b711c92f53970fa543978
OtakuKenz <38698133+OtakuKenz@users.noreply.github.com>
2023-11-12 16:16:33 +0900

    Changed to modal

--- POS Web/Pages/Item/ItemCategory.razor.cs
+++ POS Web/Pages/Item/ItemCategory.razor.cs
@@ -1,34 +1,65 @@
 ﻿using System.Net.Http.Json;
+using BlazorBootstrap;
 using CommonLibrary.APIRoutes.Item;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace POS_Web.Pages.Item;
 
-public partial class ItemCategory
+public partial class ItemCategory : ComponentBase
 {
     [Inject]
-    public HttpClient HttpClient { get; set; } = null!;
+    private HttpClient HttpClient { get; set; } = null!;
 
-    public CommonLibrary.Model.Item.ItemCategory ItemCategoryModel { get; set; } = new();
+    [Inject]
+    private ToastService ToastService { get; set; } = null!;
 
-    public async void OnValidFormSubmit()
+    private CommonLibrary.Model.Item.ItemCategory ItemCategoryModel { get; set; } = new();
+
+    private bool IsSaving = false;
+
+    private EditContext ItemCategoryEditContext = default!;
+
+    protected override void OnInitialized()
     {
-        try
+        ItemCategoryEditContext = new(ItemCategoryModel);
+    }
+
+
+    private async void OnValidFormSubmit()
+    {
+        if (!IsSaving)
         {
-            var result = await HttpClient.PostAsJsonAsync(ItemCategoryRoute.CreateItemCategory_FullPath, ItemCategoryModel);
-            if (result.IsSuccessStatusCode)
+            if (ItemCategoryEditContext.Validate())
             {
-                Console.WriteLine("Submitted");
-            }
-            else
-            {
-                Console.WriteLine("Failed");
-                Console.WriteLine(result.Content.ToString());
+                IsSaving = true;
+                StateHasChanged();
+                var result = await HttpClient.PostAsJsonAsync(ItemCategoryRoute.CreateItemCategory_FullPath, ItemCategoryModel);
+                if (result.IsSuccessStatusCode)
+                {
+                    ToastService.Notify(new(ToastType.Success, title: "Success", message: $"Item Category: {ItemCategoryModel.Name} saved."));
+                    ItemCategoryModel = new();
+                }
+                else
+                {
+                    ToastService.Notify(new(ToastType.Warning, title: "Error", message: $"{await result.Content.ReadAsStringAsync()}"));
+                }
+                IsSaving = false;
+                StateHasChanged();
             }
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
+    }
+
+    private Modal modal = default!;
+
+    private async Task OnShowModalClick()
+    {
+        await modal.ShowAsync();
+    }
+
+    private async Task OnHideModalClick()
+    {
+        ItemCategoryModel = new();
+        await modal.HideAsync();
     }
 }
