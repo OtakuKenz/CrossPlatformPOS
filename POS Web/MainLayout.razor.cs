59d02551a89d623e223f8060136ca4435239716e
OtakuKenz <38698133+OtakuKenz@users.noreply.github.com>
2023-11-12 23:11:58 +0900

    Changed to BlazorBootstrap

---
+++ POS Web/MainLayout.razor.cs
@@ -0,0 +1,27 @@
+﻿using BlazorBootstrap;
+
+namespace POS_Web;
+
+public partial class MainLayout
+{
+
+    Sidebar sidebar = null!;
+    IEnumerable<NavItem> navItems = null!;
+
+    private async Task<SidebarDataProviderResult> SidebarDataProvider(SidebarDataProviderRequest request)
+    {
+        navItems ??= GetNavItems();
+
+        return await Task.FromResult(request.ApplyTo(navItems));
+    }
+
+    private IEnumerable<NavItem> GetNavItems()
+    {
+        navItems = new List<NavItem>
+        {
+            new() { Id = "1", Href = "/items", IconName = IconName.HouseDoorFill, Text = "Item"},
+        };
+
+        return navItems;
+    }
+}
